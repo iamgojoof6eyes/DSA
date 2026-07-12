cf2c84c0271b7928a8e33db9799cecf49dae2681
Captain D. Ezio <iamgojoof6eyes@gmail.com>
2026-07-12 16:59:44 +0530

    new day new problem

---
+++ code/MiddleLinkedList.java
@@ -0,0 +1,30 @@
+public class MiddleLinkedList {
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this(data, null);
+        }
+
+        Node(int data, Node next) {
+            this.data = data;
+            this.next = next;
+        }
+    }
+    
+    public Node findMiddle(Node head) {
+        if (head == null) {
+            return null;
+        }
+
+        Node slow = head;
+        Node fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+}
---
+++ code/ReverseLinkList.java
@@ -0,0 +1,29 @@
+public class ReverseLinkList {
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this(data, null);
+        }
+
+        Node(int data, Node next) {
+            this.data = data;
+            this.next = next;
+        }
+    }
+
+    public static Node reverse (Node head) {
+        Node prev = null;
+        Node current = head;
+        Node next = null;
+
+        while (current != null) {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        return prev;
+    }
+}
